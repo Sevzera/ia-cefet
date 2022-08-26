9d59e5dda2d4cef18fc1886d166f8ea970432a78
Arthur Severo <62119941+Sevzera@users.noreply.github.com>
2022-08-26 11:16:02 -0300

    deus abencoe o js

--- atv1_busca-A/src/main.js
+++ atv1_busca-A/src/main.js
@@ -1,24 +1,24 @@
-import { readCSV } from './utils/csvReader.js';
-import { generateNodes } from './createNode.js';
-import readline from 'readline';
+import fs from "fs";
+import { generateNodes } from "./nodeHandler.js";
+import readline from "readline";
 
-const heuristics_data = await readCSV("heuristics.csv");
-const real_distances_data = await readCSV("real-distances.csv");
-console.log(heuristics_data, real_distances_data);
-const nodes = generateNodes(heuristics_data.length, heuristics_data, real_distances_data);
+const heuristics_data = await JSON.parse(
+	fs.readFileSync("../data/heuristics.json", "utf-8")
+);
+const real_distances_data = await JSON.parse(
+	fs.readFileSync("../data/real-distances.json", "utf-8")
+);
+const nodes = generateNodes(
+	heuristics_data.length,
+	heuristics_data,
+	real_distances_data
+);
 console.log(nodes);
-
-let finalIndex;
-input.question("Digite o índice final: ", (answer) => {
-	finalIndex = parseInt(answer);
-	input.close();
-});
-
-console.log(finalIndex);
 
 // let finalIndex;
 // input.question("Digite o índice final: ", (answer) => {
 // 	finalIndex = parseInt(answer);
 // 	input.close();
 // });
+
 // console.log(finalIndex);
--- atv1_busca-A/src/nodeHandler.js
+++ atv1_busca-A/src/nodeHandler.js
@@ -3,17 +3,17 @@ const createNode = (heuristic) => {
 		heuristic: [],
 		real_distance: [],
 		visited: false,
-
-
 	};
 };
 
-export const generateNodes = (number_of_nodes, heuristic_data, real_distances_data) => {
+export const generateNodes = (
+	number_of_nodes,
+	heuristic_data,
+	real_distances_data
+) => {
 	let nodes = [];
 	for (let i = 0; i < number_of_nodes; i++) {
 		nodes.push(createNode(heuristic_data[i], real_distances_data[i]));
 	}
 	return nodes;
 };
-
-
