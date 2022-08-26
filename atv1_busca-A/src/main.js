5549e0d3367a973e42a627e316846dbca58f39b1
Victor Le Roy Matos <victorlrmatos@gmail.com>
2022-08-26 08:05:07 -0300

    main + csvReader

--- atv1_busca-A/src/main.js
+++ atv1_busca-A/src/main.js
@@ -1,4 +1,6 @@
+import { readCSV } from "./utils/csvReader.js";
 import readline from 'readline';
+
 const input = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
@@ -9,3 +11,6 @@ const finalIndex = input.question('Digite o índice final: ', (answer) => {
 });
 
 console.log(finalIndex);
+
+const heuristics_data = readCSV("heuristics.csv");
+const real_distances_data = readCSV("real_distances.csv");
