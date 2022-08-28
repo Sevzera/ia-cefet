46fc3b044a2bc4de41f8f2e9fb686b53cc3d914e
Arthur Severo <62119941+Sevzera@users.noreply.github.com>
2022-08-28 14:13:20 -0300

    reafatoracao

--- atv1_busca-A/src/searchHandler.js
+++ atv1_busca-A/src/searchHandler.js
@@ -3,29 +3,29 @@ export const search = (nodes, start, end) => {
 	let currentNode = nodes[start];
 	currentNode.visited = true;
 
-	let currentCost = 0;
+	let pathCost = 0;
 	let leastCost = Number.MAX_SAFE_INTEGER;
 	let queue = [currentNode];
 	let border = currentNode.neighbours;
 
-	while (currentNode != finalNode) {
+	do {
 		border.forEach((relation) => {
-			const newCost = currentCost + relation.cost;
+			const newCost = pathCost + relation.cost;
 			const heuristicCost = newCost + relation.node.heuristic[end];
 			if (heuristicCost < leastCost && !relation.node.visited) {
 				leastCost = heuristicCost;
 				currentNode = relation.node;
 			}
 		});
-
-		currentCost = leastCost - currentNode.heuristic[end];
+		pathCost = leastCost - currentNode.heuristic[end];
 		leastCost = Number.MAX_SAFE_INTEGER;
 		currentNode.visited = true;
 		queue.push(currentNode);
-		let newBorder = [...border, ...currentNode.neighbours].filter(
-			(relation) => !relation.node.visited
-		);
+		let newBorder = [
+			...border,
+			...currentNode.neighbours.filter((relation) => !relation.node.visited),
+		];
 		border = newBorder;
-	}
-	return [queue, currentCost];
+	} while (currentNode != finalNode);
+	return [queue, pathCost];
 };
