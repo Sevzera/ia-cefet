2869dd045bbe678fbbe851305f5a4894c7f3e29a
Victor Le Roy Matos <victorlrmatos@gmail.com>
2022-08-29 16:42:25 -0300

    alteranco comentarios

--- atv1_busca-A/src/searchHandler.js
+++ atv1_busca-A/src/searchHandler.js
@@ -17,11 +17,13 @@ export const search = (nodes, start, end) => {
 		for (let i = 0; i < openList.length; i++) {
 			let currentNodeF = pathCost[currentNode.node_name] + currentNode.h_distance[end];
 			let openListF = pathCost[openList[i].node_name] + openList[i].h_distance[end];
+
 			console.log(`	Comparando ${currentNode.node_name} com ${openList[i].node_name}`);
 			console.log(`		Custo de ${currentNode.node_name}: ${currentNodeF} = custo (${pathCost[currentNode.node_name]}) + heuristica (${currentNode.h_distance[end]})` +
 				"\n"+
 				`		Custo de ${openList[i].node_name}: ${openListF} = custo (${pathCost[openList[i].node_name]}) + heuristica (${openList[i].h_distance[end]})`
 			);
+
 			if (openListF < currentNodeF) {
 				console.log(`		${openList[i].node_name} tem menor custo que ${currentNode.node_name}`);
 				currentNode = openList[i];
@@ -34,7 +36,7 @@ export const search = (nodes, start, end) => {
 		
 		console.log(`\nNode expandido atual: ${currentNode.node_name}`);
 
-		// Verificando se o node atual eh o node final
+		// Verificando se o node atual eh o node final e retornando o caminho
 		if (currentNode == finalNode) {
 			let path = [];
 			let node = currentNode;
@@ -49,27 +51,39 @@ export const search = (nodes, start, end) => {
 
 		// Expansao do node atual
 		for (let i = 0; i < currentNode.neighbors.length; i++) {
-			
-			let neighbor = currentNode.neighbors[i]; // Definindo o vizinho atual
+			// Definindo o vizinho atual
+			let neighbor = currentNode.neighbors[i]; 
 			console.log(`	Vizinho atual: ${neighbor.node.node_name}`);
-			
-			if (!closedList.includes(neighbor.node)) { // Verificando se o node vizinho nao esta na lista de nodes expandidos
-				if (!openList.includes(neighbor.node)) { // Verificando se o node vizinho nao esta na lista de nodes nao expandidos
-					neighbor.node.parent = currentNode; // Definindo o node atual como o pai do node vizinho
-					openList.push(neighbor.node); // Adicionando o node vizinho na lista de nodes nao expandidos
-					pathCost[neighbor.node.node_name] = pathCost[currentNode.node_name] + neighbor.g_distance; // Definindo o custo do node vizinho
+			// Verificando se o node vizinho nao esta na lista de nodes expandidos
+			if (!closedList.includes(neighbor.node)) { 
+				// Verificando se o node vizinho nao esta na lista de nodes nao expandidos
+				if (!openList.includes(neighbor.node)) {
+					// Definindo o node atual como o pai do node vizinho 
+					neighbor.node.parent = currentNode; 
+					// Adicionando o node vizinho na lista de nodes nao expandidos
+					openList.push(neighbor.node); 
+					// Definindo o custo do node vizinho
+					pathCost[neighbor.node.node_name] = pathCost[currentNode.node_name] + neighbor.g_distance; 
 				}
 			}
 
 			console.log(` 		Custo: ${pathCost[neighbor.node.node_name]}`);
 			
-			if (pathCost[neighbor.node.node_name] > pathCost[currentNode.node_name] + neighbor.g_distance) { // Verificando se o custo do node vizinho eh maior que o custo do node atual + a distancia do node atual para o node vizinho
-				neighbor.node.parent = currentNode; // Definindo o node atual como o pai do node vizinho
-				pathCost[neighbor.node.node_name] = pathCost[currentNode.node_name] + neighbor.g_distance; // Definindo o custo do node vizinho
+			// Verificando se o custo do node vizinho eh maior que o custo do node atual + a distancia do node atual para o node vizinho
+			if (pathCost[neighbor.node.node_name] > pathCost[currentNode.node_name] + neighbor.g_distance) { 
+				// Definindo o node atual como o pai do node vizinho
+				neighbor.node.parent = currentNode; 
+				// Definindo o custo do node vizinho
+				pathCost[neighbor.node.node_name] = pathCost[currentNode.node_name] + neighbor.g_distance; 
+
 				console.log(`		Custo do vizinho e pai atualizado! Custo: ${pathCost[neighbor.node.node_name]} -- Pai: ${neighbor.node.parent.node_name}`);
-				if (closedList.includes(neighbor.node)) { // Verificando se o node vizinho esta na lista de nodes expandidos
-					closedList = closedList.filter((node) => node != neighbor.node); // Removendo o node vizinho da lista de nodes expandidos
-					openList.push(neighbor.node); // Adicionando o node vizinho na lista de nodes nao expandidos
+
+				// Verificando se o node vizinho esta na lista de nodes expandidos
+				if (closedList.includes(neighbor.node)) { 
+					// Removendo o node vizinho da lista de nodes expandidos
+					closedList = closedList.filter((node) => node != neighbor.node); 
+					// Adicionando o node vizinho na lista de nodes nao expandidos
+					openList.push(neighbor.node); 
 				}
 			}
 
