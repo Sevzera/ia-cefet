0dc23c6b069483003e769f2214f2c35078d181e4
Victor Le Roy Matos <victorlrmatos@gmail.com>
2022-10-13 14:34:03 -0300

    cabeca da explodindo, apenas

--- atv5_redes_neurais/src/NeuralNetwork.py
+++ atv5_redes_neurais/src/NeuralNetwork.py
@@ -7,38 +7,43 @@ class NeuralNetwork:
     def __init__(self, learning_rate) -> None:
         self.learning_rate = learning_rate        
 
-    def sigmoid(x):
+    def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
-    def step(x):
+    def step(self, x):
+        print(x)
         return 1 if x >= 0 else 0
     
     def perceptron_step(self, max_it, inputs, targets):
         weights = []
+        for i in range(len(inputs)):
+            weights.append(np.array([np.random.uniform(-1, 1) for i in range(len(inputs[i][0]))]))
+        # print(weights, len(weights))
+        
         bias = []
-        t = 1
-        error = 1
-        while t < max_it and error > 0:
-            error = 0
-            for i in range(len(inputs)):
-                output = self.step(np.dot(weights, inputs[i]) + bias)
-                error += abs(targets[i] - output)
-                weights += self.learning_rate * (error) * inputs[i]
-                bias += self.learning_rate * (error)
-            t += 1
-        return weights, bias
+        for i in range(len(inputs)):
+            bias.append(np.array([np.random.uniform(-1, 1) for i in range(len(inputs[i][0]))]))
+        # print(bias, len(bias))
 
-    def perceptron_sigmoid(self, max_it, inputs, targets):
-        weights = []
-        bias = []
-        t = 1
-        error = 1
-        while t < max_it and error > 0:
-            error = 0
-            for i in range(len(inputs)):
-                output = self.sigmoid(np.dot(weights, inputs[i]) + bias)
-                error += abs(targets[i] - output)
-                weights += self.learning_rate * (error) * inputs[i]
-                bias += self.learning_rate * (error)
-            t += 1
+        # t = 0
+        # error = 1
+        # while (t < max_it and error > 0):
+        #     error = 1
+        #     for i in range(len(inputs)):
+        #         x = inputs[i][0]                
+        #         d_name = inputs[i][1]
+        #         d = [d for d in targets if d[1] == d_name][0][0]
+        #         print(d)
+                
+        #         x1 = np.dot(x, weights) + bias
+        #         y = self.step(x1)
+        #         # print("y = " + str(y))
+        #         # print("calc = " + str(x1))
+                
+        #         # error += d - y
+        #         # weights += self.learning_rate * error * np.transpose(x)
+        #         # bias += self.learning_rate * error * 1
+        #     t += 1
+
         return weights, bias
+       
--- atv5_redes_neurais/src/main.py
+++ atv5_redes_neurais/src/main.py
@@ -1,9 +1,32 @@
 import pandas as pd
 
+from NeuralNetwork import NeuralNetwork
+
 def main():
     df_path = 'data/'
     df = pd.read_csv(df_path + 'column_3C.dat', sep=' ', header=None)
-    print(df)
+    # print(df)
+
+    d_target_name = df[6].unique()
+    d_target = []
+    for i in reversed(range(d_target_name.size)):
+        aux = []
+        for j in range(d_target_name.size):
+            if(i == j): aux.append(1)
+            else: aux.append(0)
+        d_target.append( [aux, d_target_name[i]] )
+    # print(d_target)
+
+    x_input_df = df.iloc[:, 0:6].values
+    x_input = []
+    for i in range(len(x_input_df)):
+        x_input.append( [x_input_df[i].tolist(), df[6][i]] )
+    # print(x_input)
+
+    rna = NeuralNetwork(0.1)
+    weight, bias = rna.perceptron_step(100, x_input, d_target)
+    # print(weight, bias)
+
     pass
 
 if __name__ == '__main__':
