a47541c54596c8213907015defcce4fa59179ba7
Arthur Severo <62119941+Sevzera@users.noreply.github.com>
2022-11-01 12:00:10 -0300

    terminado em js

--- atv6_algoritmo_genetico/js/src/individual.js
+++ atv6_algoritmo_genetico/js/src/individual.js
@@ -2,16 +2,27 @@ export default class Individual {
 	constructor(x, y) {
 		this.x = Number(x);
 		this.y = Number(y);
-		this.fitness = null;
+		this.fitness = this.calculateFitness();
 		this.mappedFitness = null;
 	}
 
-	calculateFitness(fitnessFunction) {
-		this.fitness = Number(fitnessFunction(this.x, this.y));
+	calculateFitness() {
+		return Number(
+			(
+				Math.sin(this.x) * Math.exp((1 - Math.cos(this.y)) ** 2) +
+				Math.cos(this.y) * Math.exp((1 - Math.sin(this.x)) ** 2) +
+				(this.x - this.y) ** 2
+			).toFixed(2)
+		);
 	}
 
-	calculateMappedFitness(linearMappingFunction, i) {
-		this.mappedFitness = Number(linearMappingFunction(i));
+	tryToMutate() {
+		if (Math.random() < 0.05) {
+			this.x = Number((Math.random() * 20 - 10).toFixed(5));
+		}
+		if (Math.random() < 0.05) {
+			this.y = Number((Math.random() * 20 - 10).toFixed(5));
+		}
 	}
 
 	toString() {
--- atv6_algoritmo_genetico/js/src/main.js
+++ atv6_algoritmo_genetico/js/src/main.js
@@ -1,18 +1,18 @@
 import Individual from "./individual.js";
 
 const N = 100;
+const Nsum = 5050;
 
-const fitnessFunction = (x, y) => {
-	return (
-		Math.sin(x) * Math.exp((1 - Math.cos(y)) ** 2) +
-		Math.cos(y) * Math.exp((1 - Math.sin(x)) ** 2) +
-		(x - y) ** 2
-	).toFixed(2);
-};
-const linearMappingFunction = (i) => {
-	const max = 100;
-	const min = 0;
-	return min + (max - min) * ((N - i) / (N - 1)).toFixed(2);
+const crossover = (mother, father) => {
+	const motherXRate = Math.random();
+	const fatherXRate = 1 - motherXRate;
+	const x = (motherXRate * mother.x + fatherXRate * father.x).toFixed(5);
+
+	const motherYRate = Math.random();
+	const fatherYRate = 1 - motherXRate;
+	const y = (motherYRate * mother.y + fatherYRate * father.y).toFixed(5);
+
+	return new Individual(x, y);
 };
 
 let population = [];
@@ -20,20 +20,55 @@ let population = [];
 for (let i = 0; i < N; i++) {
 	population.push(
 		new Individual(
-			(Math.random() * 20 - 10).toFixed(2),
-			(Math.random() * 20 - 10).toFixed(2)
+			(Math.random() * 20 - 10).toFixed(5),
+			(Math.random() * 20 - 10).toFixed(5)
 		)
 	);
-	population[i].calculateFitness(fitnessFunction);
 }
-
 population.sort((a, b) => {
 	return a.fitness - b.fitness;
 });
 for (let i = 0; i < N; i++) {
-	population[i].calculateMappedFitness(linearMappingFunction, i + 1);
+	population[i].mappedFitness = N - i;
 }
 
-population.forEach((individual) => {
-	console.log(individual.toString());
-});
+let children = [];
+let n = 0;
+
+while (n < 1000) {
+	let pairs = [];
+	for (let i = 0; i < N; i++) {
+		const odd = Math.round(Math.random() * Nsum);
+		let j;
+		let sum = 0;
+		for (j = 1; j < N; j++) {
+			sum += j;
+			if (sum >= odd) break;
+		}
+		pairs[i] = population.find((individual) => individual.mappedFitness === j);
+	}
+
+	for (let i = 0; i < N; i += 2) {
+		if (Math.random() < 0.7) {
+			children[i] = crossover(pairs[i], pairs[i + 1]);
+			children[i + 1] = crossover(pairs[i], pairs[i + 1]);
+		} else {
+			children[i] = new Individual(pairs[i].x, pairs[i].y);
+			children[i + 1] = new Individual(pairs[i + 1].x, pairs[i + 1].y);
+		}
+		children[i].tryToMutate();
+		children[i + 1].tryToMutate();
+	}
+
+	children.sort((a, b) => {
+		return a.fitness - b.fitness;
+	});
+	for (let i = 0; i < N; i++) {
+		children[i].mappedFitness = N - i;
+	}
+	population = children;
+
+	n++;
+}
+console.log(population);
+console.log("coords:", population[0].x, population[0].y);
